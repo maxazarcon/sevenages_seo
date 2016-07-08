db894676bbf80e589412377f646f584bd1ebbf95
Max Azarcon <contact@maxazarcon.com>
2016-07-08 14:43:20 -0400

    Reset Boilerplate

---
+++ includes/class-sevenages_seo-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://maxazarcon.com/
+ * @since      1.0.0
+ *
+ * @package    Sevenages_seo
+ * @subpackage Sevenages_seo/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sevenages_seo
+ * @subpackage Sevenages_seo/includes
+ * @author     Max Azarcon <contact@maxazarcon.com>
+ */
+class Sevenages_seo_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
